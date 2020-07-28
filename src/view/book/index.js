a001ef07bc106793eced400d59ae0144c5d365f1
Jiayujessie <jiayu.li@thoughtworks.com>
2020-07-28 15:58:14 +0800

    Create the book pages and create the card component

--- src/view/book/index.js
+++ src/view/book/index.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import{ Card} from 'antd';
+import data from "./data";
+
+//https://ant.design/components/card-cn/#header
 
 class Book extends Component{
     render(){
-        return "教程"
+        return (
+            <div className="wrap">
+                {data.map((item,index)=>(
+                    <Card 
+                    title={item.title}
+                    inner = "type"
+                    >
+                        <div
+                            dangerouslySetInnerHTML ={{
+                                __html:item.content
+                            }}
+                        >
+                        </div>
+                    </Card>
+                ))}
+{/*                 <Card 
+                    title="卡片标题"
+                    loading={true}
+                    type = "inner"
+                >
+                    卡片内容
+                </Card> */}
+            </div>
+        )
     };
 }
 export default Book;
